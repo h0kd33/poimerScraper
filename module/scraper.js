f7d8fee796eab2dd137643ba0a87ed010fea7832
JohnsonChung <johnson780510@gmail.com>
2017-07-27 18:49:13 +0800

    綜合改版 scraper 粗糙的修正了一下

--- module/scraper.js
+++ module/scraper.js
@@ -17,35 +17,15 @@ function scraper(html) {
      *  所以不會被 .children() 或 .prevUntil() 抓到
      *  */
 
-    // body > 的主文章純文字資訊
-    var textNodePart = $('body').clone().find('table').remove().end().text().match(/(\d{2}\/\d{2}\/\d{2}\(.\)\d{2}:\d{2}:\d{2}.*No\.\d+)/g);
-
+    // post-head-information (無題17/07/27(四)00:33:14 ID:s7p4i63sNo.9535821del[回應])
+    var $contents = $('#contents');
+    var phi = $contents.find('.post-head').clone();
     var regex = {
-        time: /(\d{2}\/\d{2}\/\d{2}\(.\)\d{2}:\d{2}:\d{2})/,
+        time: /(\d{2}\/\d{2}\/\d{2}\(.\)\d{2}:\d{2}:\d{2})/g,
         id: /ID:([A-z\w\d\/\^\.]*\S)/g
     };
-    var mainPostTimeArray = textNodePart.toString().match(regex.time);
-    var mainPostIDArray =  textNodePart.toString().match(regex.id);
-
-    /**
-     *  本文
-     * */
-    var mainContent = $('br[clear=left]+ hr');
-
-    // 自本文分割每篇文章(section)
-    var sectionArray = [];
-    mainContent.each(function (i, ele) {
-        var $div = $('<section>');
-
-        // reverse section from prevUntil
-        $(this).prevUntil('hr').each(function (i, ele) {
-            $div.prepend(ele);
-        });
-
-        sectionArray.push($div);
-    });
-
-    $.root().empty().append(sectionArray);
+    var mainPostTimeArray = phi.find('.now').text().match(regex.time);
+    var mainPostIDArray =  phi.find('.now').text().match(regex.id);
 
     /**
      * 各區塊獨立取出資料
@@ -58,7 +38,7 @@ function scraper(html) {
 
     var packPromises = [];
     var pack = [];
-    $('section').each(function (i, ele) {
+    $('.thread').each(function (i, ele) {
         packPromises.push(new Promise(function (resolve, reject) {
             var section = $(ele);
             var thumb = section.find('a[target=_blank] > img');
@@ -66,33 +46,31 @@ function scraper(html) {
             // Build Json Object
 
             var promises = [];
+            var $threadpost = section.find('.post.threadpost');
             var article = {
-                serial: section.children('input:checkbox').attr('name'),
-                text: section.children('blockquote').html(),
-                title: section.children('font[color=#cc1105]').find('b').html(),
-                name: section.children('font[color=#117743]').find('b').html(),
-                id: mainPostIDArray[i].replace('ID:',''),
-                time: mainPostTimeArray[i],
+                serial: $threadpost.find('.qlink').attr('data-no'),
+                text: $threadpost.find('.quote').text(),
+                title: $threadpost.find('.title').innerText,
+                id: $threadpost.find('.now').text().match(regex.id)[0],
+                time: $threadpost.find('.now').text().match(regex.time)[0],
                 thumb: thumb.attr('src'),
-                image: section.children('a:has(img)').attr('href'),
-                responseCounts: section.children('font[color=#707070]').html(),
+                image: thumb.parent().attr('href'),
+                responseCounts: $threadpost.find(".warn_txt2").innerText,
                 response: []
             };
             if (article.thumb) {
                 promises.push(sizeOfPromise(article));
             }
 
-            section.children('table').each(function (i, ele) {
-                var table = $(this);
+            section.find('.post.reply').each(function (i, ele) {
+                var $post = $(this);
                 var post = {
-                    serial: table.find('input:checkbox').attr('name'),
-                    text: table.find('blockquote').html(),
-                    title: table.find('font[color=#cc1105]').find('b').html(),
-                    name: table.find('font[color=#117743]').find('b').html(),
-                    id: table.text().match(regex.id)[0].replace('ID:',''),
-                    time: table.text().match(regex.time)[0],
-                    thumb: table.find('img').attr('src'),
-                    image: table.find('a:has(img)').attr('href')
+                    serial: $post.find('.qlink').attr('data-no'),
+                    text: $post.find('.quote').text(),
+                    id: $post.find('.now').text().match(regex.id)[0],
+                    time: $post.find('.now').text().match(regex.time)[0],
+                    thumb: $post.find('img').attr('src'),
+                    image: $post.find('img').parent().attr('href')
                 };
                 article.response.push(post);
                 if (post.thumb) {
@@ -123,6 +101,7 @@ function scraper(html) {
     packPromises.forEach(function (promise) {
         resolver = resolver.then(promise);
     });
+    console.log(pack);
     return resolver.then(function () {
         return pack;
     });
--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ var scraper = require('../module/scraper');
 /* GET home page. */
 router.get('/', function (req, res, next) {
     console.log('Request page: ' + req.query.page);
-    var targetHost = 'http://homu.komica.org/00/';
+    var targetHost = 'http://rem.komica2.net/00/';
     var targetUrl = req.query.page === '0' || req.query.page === undefined ? targetHost + 'index.htm': targetHost + req.query.page + '.htm';
 
     console.log(targetUrl);
